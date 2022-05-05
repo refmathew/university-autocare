0421d441f43f213062ccf039ac5aa23eaf74c449
Mathew Reforma <reformamathew@protonmail.com>
2022-05-05 10:20:42 +0800

    Add alt text for feature image

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,33 +1,29 @@
 <template>
   <Layout :is-home-page="false">
-    <div class="blog-wrapper page-section ">
+    <div class="blog-wrapper page-section">
       <main class="blog">
         <h1 class="blog__title">{{ $page.post.title }}</h1>
         <div class="blog__meta-info">
           <span class="blog__date">{{ `${$page.post.date} &bull; ` }}</span>
           <span class="blog__time-to-read">{{ ` ${$page.post.readTime} min read` }}</span>
         </div>
-        <g-image class="blog__feature-img" :src="$page.post.featureImg" />
+        <g-image class="blog__feature-img" :src="$page.post.featureImg" alt="Blog feature image" />
         <div class="blog__content markdown-body" v-html="$page.post.content"></div>
       </main>
       <hr />
-      <section class="read-next">
-    <div class="blogs-wrapper">
-      <section class="blogs page-section">
-        <template v-for="post in $page.allPost.edges" >
-        <blog-card
-          v-if="$page.post.title != post.node.title"
-          :key="post.node.title"
-          :blog-title="post.node.title"
-          :blog-path="post.node.path"
-          :blog-summary="post.node.summary"
-          :blog-date="post.node.date"
-          :blog-read-time="post.node.readTime"
-          :blog-feature-img="post.node.featureImg"
-        ></blog-card>
+      <section class="blog-roll page-section">
+        <template v-for="post in $page.allPost.edges">
+          <blog-card
+            v-if="$page.post.title != post.node.title"
+            :key="post.node.title"
+            :blog-title="post.node.title"
+            :blog-path="post.node.path"
+            :blog-summary="post.node.summary"
+            :blog-date="post.node.date"
+            :blog-read-time="post.node.readTime"
+            :blog-feature-img="post.node.featureImg"
+          ></blog-card>
         </template>
-      </section>
-    </div>
       </section>
     </div>
   </Layout>
@@ -59,10 +55,10 @@ query ($path: String!){
 </page-query>
 
 <script>
-  import BlogCard from '../components/BlogCard.vue'
+import BlogCard from '../components/BlogCard.vue'
 
 export default {
-  components:{
+  components: {
     BlogCard
   },
   metaInfo() {
