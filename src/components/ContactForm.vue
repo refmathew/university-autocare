845a95ef471fda511ea2296be7f119e33da53d9a
Mathew Reforma <reformamathew@protonmail.com>
2022-05-06 20:22:17 +0800

    docs: Add comment on how to enable form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,5 +1,6 @@
 <template>
   <form class="contact-form" action="https://formsubmit.co/your@email.com" method="POST">
+    <!-- Put your email on your@email.com -->
     <input class="contact-form__name" type="text" name="name" placeholder="NAME" required />
     <input class="contact-form__email" type="email" name="email" placeholder="EMAIL" required />
     <textarea class="contact-form__message" name="message" placeholder="MESSAGE"></textarea>
